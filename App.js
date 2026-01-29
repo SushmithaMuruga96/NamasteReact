79b63eea8be7b7766b1863d2243338792d300894
SushmithaMuruga96 <s30496usi@gmail.com>
2026-01-29 10:38:26 +0400

    React basic component

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement(
-  "h1",
-  { id: "title" },
-  "Hello World from React!",
+//React Element
+const jsxheading = (
+  <h1 className="head" tabIndex="5">
+    Hello World from JSX!
+  </h1>
+);
+//React Component
+const HeadingComponent = () => {
+  return (
+    <h1 className="head" tabIndex="5">
+      Hello World from Component!
+    </h1>
+  );
+};
+
+// both are same
+const HeadingComponent2 = () => (
+  <h1 className="head" tabIndex="5">
+    Hello World from Component!
+  </h1>
 );
 
-const heading2 = <h1>Hello World from JSX!</h1>;
+// react component is nothing but a function which returns a react element
 const root = ReactDOM.createRoot(document.getElementById("content"));
-root.render(heading2); //render method takes react element as an argument and coverts it to DOM element
+root.render(<HeadingComponent />); //render method takes react element as an argument and coverts it to DOM element
