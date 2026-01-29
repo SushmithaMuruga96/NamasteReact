e69c5d43b8234a01304d607f6bd974878efc882d
SushmithaMuruga96 <s30496usi@gmail.com>
2026-01-29 09:43:01 +0400

    Laying foundation part -1

--- App.js
+++ App.js
@@ -1,48 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/*
-* <div id=="parent">
-    <div id ="child">
-      <h1> I am h1 tag</h1> 
-      <h2> I am h2 tag</h2> ---> siblings
-    </div>
-    <div id ="child">
-      <h1> I am h1 tag</h1> 
-      <h2> I am h2 tag</h2> ---> siblings
-    </div>
-</div>
-*
-*
-how to create the above structure in React
+const heading1 = React.createElement(
+  "h1",
+  { id: "title" },
+  "Hello World from React!",
+);
 
-*/
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am sushmitha"),
-    React.createElement("h2", {}, "this is h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-]);
-
-//this is actuall react code, this would be structure of the react element
-// React.createElement("tag", { attribute object }, "children");
-// React.createElement("h1", { id: "heading" }, "Hello from React!!");
-
-//now to overcome the problem of writing such large code we have JSX
-
-// const heading = React.createElement(
-//   "h1",
-//   {
-//     // attribute object
-//     id: "heading", // attribute
-//     xyz: "abc", // custom attribute
-//   },
-//   "Hello from React!!"
-// );
+const heading2 = <h1>Hello World from JSX!</h1>;
 const root = ReactDOM.createRoot(document.getElementById("content"));
-root.render(parent); //render method takes react element as an argument and coverts it to DOM element
+root.render(heading2); //render method takes react element as an argument and coverts it to DOM element
