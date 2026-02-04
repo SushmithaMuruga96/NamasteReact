193069e6257693e47431dc53ea121589fbea1d78
SushmithaMuruga96 <s30496usi@gmail.com>
2026-02-04 11:15:18 +0400

    created header, body for food ordering app

--- App.js
+++ App.js
@@ -1,36 +1,98 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./App.css";
+/*
+-header
+  -logo
+  -nav
+-body
+  -search
+  -restoContainer
+    -restoCard
+      -image
+      -restoName
+      -cuisine
+      -rating
+      -deliveryTime
+      -costForTwo
+-footer
+  -links
+  -copyright
+*/
 
-const Title = () => <h1 className="title">This is an Title Component</h1>;
-
-const elem = <span>This is JSX Element</span>;
-
-const titleElement = (
-  <h1 className="title" tabIndex="0">
-    {elem} &nbsp; This is an Title Element
-  </h1>
-);
-
-//component composition - combining multiple components into component
-// javascrtip expression - any valid set of literals,
-// variables, operators,
-//js code statements
-// and expressions can be written inside curly braces {}
-
-const name = "John Doe";
-const HeadingComponent = () => {
+const RestoCard = () => {
   return (
-    <div id="container">
-      <Title />
-      <Title></Title>
-      {Title()}
-      <h1 className="head" tabIndex="5">
-        Hello from Functional Component!
-      </h1>
+    <div className="resto-card">
+      <img
+        src="https://images.deliveryhero.io/image/talabat/MenuItems/MUTTON_BIRIYANI638652657254569152.jpg"
+        alt="resto-image"
+        className="resto-image"
+      />
+      <h3 className="resto-name">Meghana Foods</h3>
+      <p className="cuisine">South Indian, North Indian, Chinese</p>
+      <p className="rating">Rating: 4.5</p>
+      <p className="delivery-time">Delivery Time: 30 mins</p>
+      <p className="cost-for-two">Cost for Two: ₹200</p>
+    </div>
+  );
+};
+const Restaurants = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <h2>Restaurants</h2>
+      <div className="resto-container">
+        <RestoCard />
+        <RestoCard />
+        <RestoCard />
+        <RestoCard />
+        <RestoCard />
+        <RestoCard />
+        <RestoCard />
+        <RestoCard />
+        <RestoCard />
+        <RestoCard />
+      </div>
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img
+        src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"
+        alt="logo"
+        className="logo"
+      />
+      <nav className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>
+            <img
+              src="https://p7.hiclipart.com/preview/1022/32/945/shopping-cart-logo-icon-shopping-cart.jpg"
+              alt="cart"
+              className="cart-icon"
+            />
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Restaurants />
+      {/* <Footer />  */}
     </div>
   );
 };
 
 // react component is nothing but a function which returns a react element
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />); //render method takes react element as an argument and coverts it to DOM element
+root.render(<AppLayout />); //render method takes react element as an argument and coverts it to DOM element
